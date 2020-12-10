e5fe10027447f8969af741ed536ffd30f80fbf6e
seunghwan <ghks285@naver.com>
2020-12-10 18:24:18 +0900

    백준 -10808번

--- Backjoon/src/alpa_find.java
+++ Backjoon/src/alpa_find.java
@@ -1,9 +1,26 @@
+import java.util.Scanner;
 
 public class alpa_find {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		Scanner sc=new Scanner(System.in);
+		
+		String s=sc.next();
+		
+		int arr[]=new int[26];
+		
+		for(int i=0; i<s.length(); i++)
+		{
+			arr[s.charAt(i)-97]++;
+		}
+		
+		
+		
+		for(int i=0; i<arr.length; i++) {
+			System.out.print(arr[i]+" ");
+		}
 	}
 
 }
