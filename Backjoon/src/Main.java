ffa07ccd58ef9714f87f355aa80faf39f8bab065
seunghwan <ghks285@naver.com>
2020-12-13 14:06:13 +0900

    백준-10798 세로읽기

--- Backjoon/src/Main.java
+++ Backjoon/src/Main.java
@@ -1,12 +1,34 @@
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("git 오류나서 다시 만드는 프로젝트");
+
+
+		
+		Scanner sc=new Scanner(System.in);
+		
+		char c[][]=new char[5][15];
+		
+		for(int i=0; i<c.length; i++) {
+			String s=sc.next();
+			
+			for(int j=0; j<s.length(); j++) {
+				c[i][j]=s.charAt(j);
+			}
+		}
 		
 		
-		
+		for(int i=0; i<15; i++) {
+			for(int j=0; j<5; j++) {
+				if(c[j][i]==' '|| c[j][i]=='\0') {
+					continue;
+				}
+				
+				System.out.print(c[j][i]);
+			}
+		}
 		
 	}
 
