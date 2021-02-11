752b018ada2e78131efcb1be372d750244be5e10
seunghwan <ghks285@naver.com>
2021-02-11 13:22:02 +0900

    백준 1550번 16진수

---
+++ Backjoon/src/bronze_1550.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class bronze_1550 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+		
+		Scanner sc=new Scanner(System.in);
+		
+		String n=sc.next();
+		System.out.println(Integer.parseInt(n,16));
+		
+	}
+
+}
