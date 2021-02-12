a5228b8cd5b17672e6c1f677b242d90174397f69
seunghwan <ghks285@naver.com>
2021-02-12 16:47:03 +0900

    백준 6749번 Next in line

---
+++ Backjoon/src/bronze_6749.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class bronze_6749 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner sc=new Scanner(System.in);
+		
+		//1. 12 15 18 =3
+		//2. 5 10 15 =5
+		int n1=sc.nextInt();
+		int n2=sc.nextInt();
+		
+		
+		//3. 15-12 =3 
+		int res=n2-n1;
+		
+		int sum=0;
+		sum=n2+res;
+		System.out.println(sum);
+		
+		//4. 15+3=18
+		
+		
+	}
+
+}
