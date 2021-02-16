eaa0c5dadcb2d57af33ea387d0a2d65a0aaac905
seunghwan <ghks285@naver.com>
2021-02-16 23:37:49 +0900

    백준 11283번 한글 2

---
+++ Backjoon/src/bronze_11283.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class bronze_11283 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	
+		char h;
+		
+		Scanner sc=new Scanner(System.in);
+		
+		h=sc.nextLine().charAt(0);
+		
+		
+		System.out.println((int)h-44031);
+		
+	}
+
+}
