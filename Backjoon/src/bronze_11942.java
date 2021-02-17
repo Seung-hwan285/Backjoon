551671f97215b15118ff0b05c2e6b7ce346fa882
seunghwan <ghks285@naver.com>
2021-02-17 16:09:56 +0900

    백준 11942번 고려대는 사랑입니다

---
+++ Backjoon/src/bronze_11942.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class bronze_11942 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		
+		
+		 String []arr= {"고려대학교"};
+		 
+
+		 
+		 for(int i=0; i<arr.length; i++) {
+			 System.out.println(arr[i]);
+		 }
+	}
+
+}
