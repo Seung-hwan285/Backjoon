a05441d6ad44ec09be0a1578ba2ba0e88523c6ba
seunghwan <ghks285@naver.com>
2021-02-17 18:05:00 +0900

    백준 14645 와이버스 부릉부릉

---
+++ Backjoon/src/bronze_14645.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class bronze_14645 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		Scanner sc=new Scanner(System.in);
+		
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		
+		for(int i=0; i<a; i++) {
+			int one=sc.nextInt();
+			int two=sc.nextInt();
+		}
+		
+		System.out.println("비와이");
+		
+	}
+
+}
