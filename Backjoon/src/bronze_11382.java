fbf0c1d46445c0e224eca685275d8da6c2ca55d1
seunghwan <ghks285@naver.com>
2021-02-17 16:07:29 +0900

    백준 11382번 꼬마 정민

---
+++ Backjoon/src/bronze_11382.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class bronze_11382 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		Scanner sc=new Scanner(System.in);
+		
+		
+		System.out.println(sc.nextLong()+sc.nextLong()+sc.nextLong());
+	}
+
+}
