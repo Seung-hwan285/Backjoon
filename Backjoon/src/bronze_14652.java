0f8f3500f675b648c0b6c2533ef99fba91759d3b
seunghwan <ghks285@naver.com>
2021-02-19 21:30:02 +0900

    백준 14652번 나는 행복합니다~

---
+++ Backjoon/src/bronze_14652.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class bronze_14652 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		Scanner sc=new Scanner(System.in);
+		
+		int row=sc.nextInt();
+		int colum=sc.nextInt();
+		int num=sc.nextInt();
+		
+		
+		System.out.println(num/colum+" "+num%colum);
+
+	}
+
+}
