22f23c819713dce647c154d1d53dfb5070fdf9c2
seunghwan <ghks285@naver.com>
2021-02-20 20:21:51 +0900

    백준 15439번 Vera and Outfits

---
+++ Backjoon/src/bronze_15439.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class bronze_15439 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	
+		
+	Scanner sc=new Scanner(System.in);
+	
+	int n=sc.nextInt();
+	
+	System.out.println(n*(n-1));
+		
+	
+	}
+
+}
