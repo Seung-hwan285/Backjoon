1db1d24ef4844241ba0dd7595703800257b19fb3
seunghwan <ghks285@naver.com>
2021-02-21 17:31:15 +0900

    백준 15894번 수학은 체육과목입니다

---
+++ Backjoon/src/bronze_15894.java
@@ -0,0 +1,38 @@
+	import java.util.Scanner;
+
+public class bronze_15894 {
+
+
+	
+
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+
+			//n 1 = 4
+			//n 2 = 8
+			//n 3 = 12
+//				n 4 = 16
+			
+			//n+1 sum+4
+			
+			Scanner sc=new Scanner(System.in);
+			
+			long n=sc.nextLong();
+			long sum=0;
+			
+			for(int i=0; i<n; i++) {
+				
+				sum+=4;
+			}
+			
+			System.out.println(sum);
+			
+		
+			
+			
+			
+			
+			
+		}
+
+	}
