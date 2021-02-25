56e1382bca5ab0540248c1d88ca8b5d84f77705a
seunghwan <ghks285@naver.com>
2021-02-25 19:21:23 +0900

    백준 17256번 달달함이 넘쳐흘러

---
+++ Backjoon/src/bronze_17256.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class bronze_17256 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		Scanner sc=new Scanner(System.in);
+		
+		int ax=sc.nextInt();
+		int ay=sc.nextInt();
+		int az=sc.nextInt();
+		
+		int cx=sc.nextInt();
+		int cy=sc.nextInt();
+		int cz=sc.nextInt();
+		
+		
+		System.out.println(cx-az);
+		System.out.println(cy/ay);
+		System.out.println(cz-ax);
+		
+		
+		
+	}
+
+}
