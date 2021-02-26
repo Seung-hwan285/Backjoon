b4ea166f43f883ebe22e0c591756171b0716059a
seunghwan <ghks285@naver.com>
2021-02-26 22:51:18 +0900

    백준 18108번 1998년생인 내가 태국에서는 2541년생?!

---
+++ Backjoon/src/bronze_18108.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class bronze_18108 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int bol=544;
+		
+		Scanner sc=new Scanner(System.in);
+		
+		int n=sc.nextInt();
+		
+		
+		System.out.println((n-bol)+1);
+		
+	}
+
+}
