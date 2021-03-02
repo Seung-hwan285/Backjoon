24c14d844fc455f840e20f925f622a4901c74c4c
seunghwan <ghks285@naver.com>
2021-03-02 23:03:07 +0900

    백준 2420 사파리월드

---
+++ Backjoon/src/bronze_2420.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class bronze_2420 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		
+		//절대값 이용
+	
+		
+		
+		
+		Scanner sc=new Scanner (System.in);
+		
+		long n=sc.nextLong();
+		long m=sc.nextLong();
+		
+		
+		System.out.println(Math.abs(n-m));
+	
+		
+		
+	}
+
+}
