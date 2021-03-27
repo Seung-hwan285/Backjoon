da9184ec934ec39af17d993c1a21cef9219bdfe8
seunghwan <ghks285@naver.com>
2021-03-27 21:03:10 +0900

    dsa

---
+++ Backjoon/src/bronze_5532.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+import java.util.Stack;
+
+public class bronze_5532 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+	
+	
+	
+		
+		
+		Scanner sc=new Scanner(System.in);
+		
+		
+		int n=sc.nextInt();
+		
+	
+		
+		Stack<Integer>s=new Stack();
+		
+		for(int i=0; i<n; i++) {
+			int n2=sc.nextInt();
+			
+			
+			if(n2==0) {
+				s.pop();
+			}
+			else {
+				s.push(n2);
+			}
+		}
+		int sum=0;
+		for(Integer i:s) {
+			sum+=i;
+		}
+		
+		System.out.println(sum);
+		
+		
+		
+		
+		
+	}
+
+}
